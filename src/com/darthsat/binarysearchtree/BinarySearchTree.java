9d482dd5ed588b7b6bf2b89326e3b56a05900675
darthsat <coldwraith@gmail.com>
2018-09-11 02:40:41 +0300

    self-balancing

--- src/com/darthsat/binarysearchtree/BinarySearchTree.java
+++ src/com/darthsat/binarysearchtree/BinarySearchTree.java
@@ -13,6 +13,10 @@ public class BinarySearchTree {
         }
     }
 
+    private int level(Node node) {
+        return node != null ? node.getLevel() : -1;
+    }
+
     public void add(int value) {
         root = addRecursive(root, value);
     }
@@ -22,16 +26,59 @@ public class BinarySearchTree {
             return new Node(value);
         }
 
-        if (value < current.value) {
-            current.left = addRecursive(current.left, value);
-        } else if (value > current.value) {
-            current.right = addRecursive(current.right, value);
+        if (value < current.getValue()) {
+            current.setLeft(addRecursive(current.getLeft(), value));
+            if (level(current.getLeft()) - level(current.getRight()) == 2) {
+                if (value < current.getLeft().getValue()) {
+                    current = rotateLeft(current);
+                } else {
+                    current = doubleRotateLeft(current);
+                }
+            }
+        } else if (value > current.getValue()) {
+            current.setRight(addRecursive(current.getRight(), value));
+            if (level(current.getRight()) - level(current.getLeft()) == 2) {
+                if (value > current.getRight().getValue()) {
+                    current = rotateRight(current);
+                } else {
+                    current = doubleRotateRight(current);
+                }
+            }
         } else {
+            current.setLevel(greaterValue(level(current.getLeft()), level(current.getRight())) + 1);
             return current;
         }
 
         return current;
 
+    }
+
+    private Node rotateLeft(Node n2) {
+        Node n1 = n2.getLeft();
+        n2.setLeft(n1.getRight());
+        n1.setRight(n2);
+        n2.setLevel(greaterValue(level(n2), level(n2.getRight())) + 1);
+        n1.setLevel(greaterValue(level(n1.getLeft()), level(n2)) + 1);
+        return n1;
+    }
+
+    private Node rotateRight(Node n1) {
+        Node n2 = n1.getRight();
+        n1.setRight(n2.getLeft());
+        n2.setLeft(n1);
+        n1.setLevel(greaterValue(level(n1.getLeft()), level(n1.getRight())) + 1);
+        n2.setLevel(greaterValue(level(n2.getRight()), level(n1)) + 1);
+        return n2;
+    }
+
+    private Node doubleRotateLeft(Node n3) {
+        n3.setLeft(rotateRight(n3.getLeft()));
+        return rotateLeft(n3);
+    }
+
+    private Node doubleRotateRight(Node n1) {
+        n1.setRight(rotateLeft(n1.getRight()));
+        return rotateRight(n1);
     }
 
     public boolean containsNode(int value) {
@@ -42,12 +89,12 @@ public class BinarySearchTree {
         if (current == null) {
             return false;
         }
-        if (value == current.value) {
+        if (value == current.getValue()) {
             return true;
         }
-        return value < current.value
-                ? containsNodeRecursive(current.left, value)
-                : containsNodeRecursive(current.right, value);
+        return value < current.getValue()
+                ? containsNodeRecursive(current.getLeft(), value)
+                : containsNodeRecursive(current.getRight(), value);
     }
 
 
@@ -60,38 +107,43 @@ public class BinarySearchTree {
             return null;
         }
 
-        if (value == current.value) {
-            if (current.left == null && current.right == null) {
+        if (value == current.getValue()) {
+            if (current.getLeft() == null && current.getRight() == null) {
                 return null;
             }
 
-            if (current.right == null) {
-                return current.left;
+            if (current.getRight() == null) {
+                return current.getLeft();
             }
 
-            if (current.left == null) {
-                return current.right;
+            if (current.getLeft() == null) {
+                return current.getRight();
             }
 
-            int smallestValue = findSmallestValue(current.right);
-            current.value = smallestValue;
-            current.right = deleteRecursive(current.right, smallestValue);
+            int smallestValue = findSmallestValue(current.getRight());
+            current.setValue(smallestValue);
+            current.setRight(deleteRecursive(current.getRight(), smallestValue));
             return current;
 
         }
-        if (value < current.value) {
-            current.left = deleteRecursive(current.left, value);
+        if (value < current.getValue()) {
+            current.setLeft(deleteRecursive(current.getLeft(), value));
             return current;
         }
-        current.right = deleteRecursive(current.right, value);
+        current.setRight(deleteRecursive(current.getRight(), value));
         return current;
     }
 
     private int findSmallestValue(Node root) {
-        return root.left == null ? root.value : findSmallestValue(root.left);
+        return root.getLeft() == null ? root.getValue() : findSmallestValue(root.getLeft());
+    }
+
+    private int greaterValue(int v1, int v2) {
+        return v1 > v2 ? v1 : v2;
     }
 
     public void traverse() {
+
         if (root == null) {
             return;
         }
@@ -103,14 +155,14 @@ public class BinarySearchTree {
 
             Node node = nodes.remove();
 
-            System.out.print(" " + node.value);
+            System.out.print(" " + node.getValue());
 
-            if (node.left != null) {
-                nodes.add(node.left);
+            if (node.getLeft() != null) {
+                nodes.add(node.getLeft());
             }
 
-            if (node.right != null) {
-                nodes.add(node.right);
+            if (node.getRight() != null) {
+                nodes.add(node.getRight());
             }
         }
         System.out.println();
--- src/com/darthsat/binarysearchtree/Node.java
+++ src/com/darthsat/binarysearchtree/Node.java
@@ -1,14 +1,52 @@
 package com.darthsat.binarysearchtree;
 
 class Node {
-    int value;
-    Node left;
-    Node right;
+    private Node left;
+    private Node right;
+    private int value;
+    private int level;
 
     Node(int value) {
         this.value = value;
+        this.level = 0;
         right = null;
         left = null;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public boolean isRoot() {
+        return getLevel() == 0;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
     }
 }
 
