f036463b8eda85ea67a5f87d8b243bf32c70ab6d
darthsat <coldwraith@gmail.com>
2018-09-15 05:23:16 +0300

    wip autobalance

--- src/com/darthsat/binarysearchtree/BinarySearchTree.java
+++ src/com/darthsat/binarysearchtree/BinarySearchTree.java
@@ -2,7 +2,6 @@ package com.darthsat.binarysearchtree;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.Queue;
 
 public class BinarySearchTree {
     Node root;
@@ -13,72 +12,28 @@ public class BinarySearchTree {
         }
     }
 
-    private int level(Node node) {
-        return node != null ? node.getLevel() : -1;
-    }
-
     public void add(int value) {
         root = addRecursive(root, value);
     }
 
     private Node addRecursive(Node current, int value) {
         if (current == null) {
-            return new Node(value);
+            current = new Node(value);
+            updateBalanceFactors();
+            current.rebalance();
+            return current;
         }
 
         if (value < current.getValue()) {
             current.setLeft(addRecursive(current.getLeft(), value));
-            if (level(current.getLeft()) - level(current.getRight()) == 2) {
-                if (value < current.getLeft().getValue()) {
-                    current = rotateLeft(current);
-                } else {
-                    current = doubleRotateLeft(current);
-                }
-            }
-        } else if (value > current.getValue()) {
-            current.setRight(addRecursive(current.getRight(), value));
-            if (level(current.getRight()) - level(current.getLeft()) == 2) {
-                if (value > current.getRight().getValue()) {
-                    current = rotateRight(current);
-                } else {
-                    current = doubleRotateRight(current);
-                }
-            }
-        } else {
-            current.setLevel(greaterValue(level(current.getLeft()), level(current.getRight())) + 1);
-            return current;
         }
 
+        if (value > current.getValue()) {
+            current.setRight(addRecursive(current.getRight(), value));
+        }
+        updateBalanceFactors();
+        current.rebalance();
         return current;
-
-    }
-
-    private Node rotateLeft(Node n2) {
-        Node n1 = n2.getLeft();
-        n2.setLeft(n1.getRight());
-        n1.setRight(n2);
-        n2.setLevel(greaterValue(level(n2), level(n2.getRight())) + 1);
-        n1.setLevel(greaterValue(level(n1.getLeft()), level(n2)) + 1);
-        return n1;
-    }
-
-    private Node rotateRight(Node n1) {
-        Node n2 = n1.getRight();
-        n1.setRight(n2.getLeft());
-        n2.setLeft(n1);
-        n1.setLevel(greaterValue(level(n1.getLeft()), level(n1.getRight())) + 1);
-        n2.setLevel(greaterValue(level(n2.getRight()), level(n1)) + 1);
-        return n2;
-    }
-
-    private Node doubleRotateLeft(Node n3) {
-        n3.setLeft(rotateRight(n3.getLeft()));
-        return rotateLeft(n3);
-    }
-
-    private Node doubleRotateRight(Node n1) {
-        n1.setRight(rotateLeft(n1.getRight()));
-        return rotateRight(n1);
     }
 
     public boolean containsNode(int value) {
@@ -95,6 +50,26 @@ public class BinarySearchTree {
         return value < current.getValue()
                 ? containsNodeRecursive(current.getLeft(), value)
                 : containsNodeRecursive(current.getRight(), value);
+    }
+
+    public void updateBalanceFactors() {
+        updateBalanceFactorRecursive(root);
+    }
+
+    private void updateBalanceFactorRecursive(Node current) {
+        if (current == null) {
+            return;
+        }
+
+        current.calculateBalanceFactor();
+
+        if (current.getLeft() != null) {
+            updateBalanceFactorRecursive(current.getLeft());
+        }
+
+        if (current.getRight() != null) {
+            updateBalanceFactorRecursive(current.getRight());
+        }
     }
 
 
@@ -142,29 +117,29 @@ public class BinarySearchTree {
         return v1 > v2 ? v1 : v2;
     }
 
-    public void traverse() {
+    void print(Node root, int space) {
+        int count = 10;
 
-        if (root == null) {
+        if (root == null)
             return;
+
+        space += count;
+
+        // Process right child first
+        print(root.getRight(), space);
+
+        // Print current node after space
+        // count
+        System.out.format("\n");
+        for (int i = count; i < space; i++) {
+            System.out.format(" ");
         }
+        System.out.format("%d\n", root.getValue());
 
-        Queue<Node> nodes = new LinkedList<>();
-        nodes.add(root);
+        print(root.getLeft(), space);
+    }
 
-        while (!nodes.isEmpty()) {
-
-            Node node = nodes.remove();
-
-            System.out.print(" " + node.getValue());
-
-            if (node.getLeft() != null) {
-                nodes.add(node.getLeft());
-            }
-
-            if (node.getRight() != null) {
-                nodes.add(node.getRight());
-            }
-        }
-        System.out.println();
+    void printTree() {
+        print(root, 0);
     }
 }
--- src/com/darthsat/binarysearchtree/Node.java
+++ src/com/darthsat/binarysearchtree/Node.java
@@ -4,25 +4,13 @@ class Node {
     private Node left;
     private Node right;
     private int value;
-    private int level;
+    private int balanceFactor;
 
     Node(int value) {
         this.value = value;
-        this.level = 0;
         right = null;
         left = null;
-    }
-
-    public int getLevel() {
-        return level;
-    }
-
-    public void setLevel(int level) {
-        this.level = level;
-    }
-
-    public boolean isRoot() {
-        return getLevel() == 0;
+        calculateBalanceFactor();
     }
 
     public Node getLeft() {
@@ -47,6 +35,100 @@ class Node {
 
     public void setValue(int value) {
         this.value = value;
+    }
+
+    public boolean hasLeft() {
+        return left != null;
+    }
+
+    public boolean hasRight() {
+        return right != null;
+    }
+
+    public boolean hasChildren() {
+        return hasLeft() || hasRight();
+    }
+
+    public int getBalanceFactor() {
+        return balanceFactor;
+    }
+
+    public int height(Node current) {
+        if (current == null) {
+            return 0;
+        }
+
+        int lheight = current.getLeft() != null ? height(current.getLeft()) : -1;
+
+        int rheight = current.getRight() != null ? height(current.getRight()) : -1;
+
+        return lheight > rheight ? lheight + 1 : rheight + 1;
+    }
+
+    public void calculateBalanceFactor() {
+        balanceFactor = height(right) - height(left);
+    }
+
+    public void rebalance() {
+
+        if (!hasChildren()) {
+            return;
+        }
+
+        if (balanceFactor == -2 && hasLeft() && left.getBalanceFactor() == -1) {
+            rotateRight(left);
+        }
+
+        if (balanceFactor == -2 && hasLeft() && left.getBalanceFactor() == 1) {
+            rotateLeftRight(left);
+        }
+
+
+        if (balanceFactor == 2 && hasRight() && getRight().getBalanceFactor() == 1) {
+            rotateLeft(right);
+        }
+
+        if (balanceFactor == 2 && hasRight() && getRight().getBalanceFactor() == -1) {
+            rotateRightLeft(right);
+        }
+    }
+
+    private void rotateLeft(Node n2) {
+        if (n2.getRight() == null) {
+            return;
+        }
+        Node n1 = n2.getRight();
+        n2.setRight(n1.getLeft());
+        n1.setLeft(n2);
+        if (n2 == left) {
+            left = n1;
+        } else {
+            right = n1;
+        }
+    }
+
+    private void rotateRight(Node n1) {
+        if (n1.getLeft() == null) {
+            return;
+        }
+        Node n2 = n1.getLeft();
+        n1.setLeft(n2.getRight());
+        n2.setRight(n1);
+        if (n1 == left) {
+            left = n2;
+        } else {
+            right = n2;
+        }
+    }
+
+    private void rotateRightLeft(Node n3) {
+        n3.rotateRight(n3.getRight());
+        rotateLeft(n3);
+    }
+
+    private void rotateLeftRight(Node n1) {
+        n1.rotateLeft(n1.getLeft());
+        rotateRight(n1);
     }
 }
 
